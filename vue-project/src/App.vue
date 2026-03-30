07f54eb2d7b576eab0135a18b3c09c58a9973882
kpl4cob <karthik.palanisamy@in.bosch.com>
2026-03-30 10:43:12 +0100

    image and bio set

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -4,33 +4,43 @@ import { ref } from 'vue'
 </script>
 
 <template>
-
   <!-- Header -->
   <h1>Karthik.P Resume</h1>
 
-  <!-- DP Image -->
-  <img class="dp_image" src="/pk_10.jpg" alt="DP" />
+  <div class="header_row">
+      <!-- DP Image -->
+      <img class="dp_image" src="/pk_10.jpg" alt="DP" />
 
-  <!-- Paragraph -->
-  <p>
-  
-  </p>
-
+      <!-- Paragraph -->
+      <p class="header_bio">
+        Specialist in, <br />
+        - AUTOSAR and Bosch Motoronic CAN/ Eth Diagnostic stack <br />
+        - Diagnostic protocols UDSonCAN, GMLAN (GMW3110), OBDonCAN (ISO 15031), <br />
+        - OBDonUDS (SAE J1979-2), DoIP (ISO 13400) <br />
+      </p>
+  </div>
 </template>
 
 <style scoped>
-h1 {
-  text-align: center;
-  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, sans-serif;
+.header_row {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.header_bio {
+  flex: 1;        /* takes up remaining space on the left */
+  margin-right: 1rem;
 }
 
 .dp_image {
   display: block;
   margin-left: auto;
-  margin-right: 0;
+  margin-right: auto;
   width: 150px;
   height: 150px;
   border-radius: 50%;
+  flex-shrink: 0; /* prevents image from being squished */
 }
 
 </style>
